dcaf2b9f65424d45d1a323f493402f4dc70baa35
Shreya <shreyashah115@gmail.com>
2021-07-09 00:40:39 -0400

    Add prevent default

--- js/accountdetails.js
+++ js/accountdetails.js
@@ -2,57 +2,58 @@ import "./firebase-authentication.js";
 import "./authentication-common.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const firstname = document.getElementById("firstname");
-    const lastname = document.getElementById("lastname");
-    const update = document.getElementById("update");
+  const firstname = document.getElementById("firstname");
+  const lastname = document.getElementById("lastname");
+  const update = document.getElementById("update");
 
-    const db = firebase.firestore();
+  const db = firebase.firestore();
 
-    let userRef = null;
+  let userRef = null;
 
-    update.addEventListener("click", function () {
-        if (firstname.value && lastname.value) {
-            updateUser(userRef.uid, firstname.value, lastname.value);
-        }
-    });
-
-    function updateUser(uid, first, last) {
-        db.collection("Users")
-            .doc(uid)
-            .update({
-                firstname: first,
-                lastname: last,
-            })
-            .then(function () {
-                console.log("User updated!");
-                alert("User details successfully updated!");
-            })
-            .catch(function (error) {
-                alert(error.message);
-                console.error(error);
-            });
+  update.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (firstname.value && lastname.value) {
+      updateUser(userRef.uid, firstname.value, lastname.value);
     }
+  });
 
-    function getUser(uid) {
-        db.collection("Users")
-            .doc(uid)
-            .get()
-            .then(function (doc) {
-                firstname.value = doc.data().firstname;
-                lastname.value = doc.data().lastname;
-            })
-            .catch(function (error) {
-                alert(error.message);
-                console.error(error);
-            });
+  function updateUser(uid, first, last) {
+    db.collection("Users")
+      .doc(uid)
+      .update({
+        firstname: first,
+        lastname: last,
+      })
+      .then(function () {
+        console.log("User updated!");
+        alert("User details successfully updated!");
+      })
+      .catch(function (error) {
+        alert(error.message);
+        console.error(error);
+      });
+  }
+
+  function getUser(uid) {
+    db.collection("Users")
+      .doc(uid)
+      .get()
+      .then(function (doc) {
+        firstname.value = doc.data().firstname;
+        lastname.value = doc.data().lastname;
+      })
+      .catch(function (error) {
+        alert(error.message);
+        console.error(error);
+      });
+  }
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      userRef = user;
+      getUser(user.uid);
+    } else {
+      window.location = "login.html";
     }
-
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            userRef = user;
-            getUser(user.uid);
-        } else {
-            window.location = "login.html";
-        }
-    });
+  });
 });
